b78b3186107c6650dc40e60dc6fa9b9b4fb5c9a4
Juraj Martinka <jumarko@gmail.com>
2023-10-06 22:59:19 +0200

    The Rust book: Guessing game - comparing guess with the secret.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
@@ -10,12 +11,21 @@ fn main() {
 
     // Processing the guess
     println!("Please enter your guess.");
-
     let mut guess = String::new();
 
     io::stdin()
+        // if the user types 5 and presses enter, guess looks like this: 5\n
         .read_line(&mut guess)
         .expect("Failed to read line");
+    // note: we are "shadowing" the original mutable variable which was a string
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}")
+    println!("You guessed: {guess}");
+
+    // compare the guess to the generated number
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!")
+    }
 }
